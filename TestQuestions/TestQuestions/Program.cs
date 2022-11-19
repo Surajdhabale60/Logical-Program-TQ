6a662302aed0a00e3ca20da474ee14b70660075c
Suraj Dhabale <surajdhabale60@gmail.com>
2022-11-19 14:07:28 +0530

    [Suraj]Add:Fibonacci Series

---
+++ TestQuestions/TestQuestions/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TestQuestions
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TestMain testMain = new TestMain();
+            testMain.Fibonacci(20);
+        }
+    }
+}
