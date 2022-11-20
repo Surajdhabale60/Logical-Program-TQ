68c3489d0939990472654dda575729d577e9c658
Surajdhabale60 <112876188+Surajdhabale60@users.noreply.github.com>
2022-11-20 23:03:19 +0530

    Update Program.cs

--- TestQuestions/TestQuestions/Program.cs
+++ TestQuestions/TestQuestions/Program.cs
@@ -1,11 +1,21 @@
-﻿namespace TestQuestions
+namespace TestQuestions
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            TestMain testMain = new TestMain();
-            testMain.Fibonacci(20);
+            while (true)
+            {
+                Console.WriteLine("\nselect Program\n 1.FibonacciSeries\n 2.ReverseNum");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        TestMain testMain = new TestMain();
+                        testMain.print();
+                        break;                   
+                }
+            }
         }
     }
 }
